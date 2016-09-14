cc6209771a1489a83782bab5d960f90805b719ac
OlgaGomez <olganoemi_1997@hotmail.com>
2016-09-14 11:25:43 -0400

    tarea05

---
+++ js/script.js
@@ -0,0 +1,21 @@
+(function(){ 
+2 	console.log('Hola Mundo'); 
+3 })(); 
+4 
+ 
+5 var descripcion = ''; 
+6 var cantidad = 0; 
+7 
+ 
+8 var array = []; 
+9 array.push('Hola Mundo'); 
+10 array.push('JavaScript'); 
+11 console.log(array); 
+12 
+ 
+13 var objeto = {} 
+14 
+ 
+15 objeto.cantidad=10; 
+16 objeto.descripcion='Descripcion'; 
+17 console.log(objeto); 
